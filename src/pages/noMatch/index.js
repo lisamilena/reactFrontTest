69a078520df1622357e0400ee49f7f9e229b19f8
Lisa Fernández <l.milena@apploading.com>
2018-05-20 19:59:23 +0200

    Create basic app structure

---
+++ src/pages/noMatch/index.js
@@ -0,0 +1,16 @@
+
+
+import React, { Component } from 'react';
+
+class NoMatch extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+          NoMatch
+      </div>
+    );
+  }
+}
+
+export default NoMatch;
